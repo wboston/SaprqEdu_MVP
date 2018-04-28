ec399b16fb60afca1e4936e232bf391b5c8777ed
William Boston <williamboston123@hotmail.com>
2018-04-27 22:32:49 -0700

    Add: UI package
    Adds Sparq template: colors picutres

---
+++ packages/SparqUIDesign/src/templates/sparquidesign/html/com_pages/page/default.php
@@ -0,0 +1,44 @@
+<? defined('KOOWA') or die('Restricted access'); ?>
+
+<? @title('SparqEdu: Phase 4 - Inspiration Powered by Connection'); ?>
+<? @description('Educational, Personal, & Career Support for Online Students'); ?>
+
+<div class="row">
+	<div class="span8 offset2">
+		<div class="hero-unit text-center">
+			<img src="https://it0vza.ch.files.1drv.com/y4m6FW7rFRk-G4Be9-_D4rAm0PGJODcq32Lm03sH6HF2TDptbmFj2afydEH-L-3IkaiaNZWDK6kgowNZ6G0N-DFt-5hPaQkjTLRO3sw3jbAq1HYaExES9rkp28ExKdnI1KlS9hWDEVbUnPg-CD_UzVZUAb4kzW3FvFUa1HAAihZtTtGYZodTWEpbICJQ1K7mOHtYDejV_tPKVObtwHM6_G-9g?width=2706&height=1717&cropmode=none" alt="SparqEdu Logo, connecting students" />
+
+			<h1>sparqEdu</h1>
+
+			<p class="lead">Inspiration Powered by Connection</p>
+
+			<p>
+				<a class="btn btn-primary" href="https://www.GetAnahita.com" target="_blank">
+					GetAnahita.com
+				</a>
+
+				<!-- <a title="Twitter" class="btn btn-twitter" href="https://www.twitter.com/anahitapolis" target="_blank">
+					<i class="icon-twitter"></i>
+				</a> -->
+
+				<a title="Facebook" class="btn btn-facebook" href="https://www.facebook.com/SPARQFORSTUDENTS/?hc_ref=ARQmK5FQvv2NyZSu71NeBNCV65ZD53wxyBPoL4In1-YTtGfOdvJ1BLexTPPhsmu991k&fref=nf" target="_blank">
+					<i class="icon-facebook"></i>
+				</a>
+
+				<!-- <a title="Google+" class="btn btn-googleplus" href="https://plus.google.com/111222489368350494929/posts" target="_blank">
+					<i class="icon-googleplus"></i>
+				</a> -->
+
+				<!-- <a title="Blog" class="btn btn-feed" href="http://blog.getanahita.com" target="_blank">
+					<i class="icon-feed"></i>
+				</a> -->
+
+				<a title="GitHub" class="btn btn-github" href="https://github.com/ckfrancisco/anahita.git" target="_blank">
+					<i class="icon-github"></i>
+				</a>
+			</p>
+
+			<p>Follow <strong>#Anahita</strong></p>
+		</div>
+	</div>
+</div>
